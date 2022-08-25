219fb4a48f4c89d722ea36b4fc0c7d1e085fa1e1
Bfabien99 <fabienbrou99@gmail.com>
2022-08-25 00:19:05 +0000

    admin can user profil

--- admin/includes/view_doctor.php
+++ admin/includes/view_doctor.php
@@ -2,8 +2,68 @@
 $doctor = getDoctorbyId($_GET["d_id"]); 
 
 if(!$doctor){
-    echo "<h4 class='alert alert-warning'>Le Docteur n'est pas reconnu</h4>";
-    echo "<p><a class='btn btn-secondary' href='./doctors.php'>Retour</a></p>";
-}
-
+    echo "<h4 class='error-box'>Le Docteur n'est pas reconnu</h4>";
+    echo "<p><a class='btn btn-primary' href='./doctors.php'>Retour</a></p>";
+}else{
 ?>
+<style>
+    .form-group{
+        font-size:1.2em;
+    }
+    h5{
+        font-size:1.5em;
+        font-weight:bold;
+    }
+</style>
+<div class="row text-center" style="background-color:white;padding:2em;">
+        <div class="col-md-12 col-lg-2">
+            <img src="./../profiles/<?php echo $doctor['picture'] ?>" style="width: 120px;height: 120px;object-fit: cover;">
+            <hr>
+        </div>
+        <div class="col-md-12 col-lg-5">
+            <div class="form-group">
+                <h5>Nom</h5>
+                <p><?php echo $doctor['first_name']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Prénom</h5>
+                <p><?php echo $doctor['last_name']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Sexe</h5>
+                <p><?php echo $doctor['sexe']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Contact1</h5>
+                <p><?php echo $doctor['contact1']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Contact2</h5>
+                <p><?php echo $doctor['contact2']?></p>
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-5">
+        <div class="form-group">
+                <h5>Email</h5>
+                <p><?php echo $doctor['email']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Pseudo</h5>
+                <p><?php echo $doctor['pseudo']?></p>
+            </div>
+        <div class="form-group">
+                <h5>Service</h5>
+                <p><?php echo $doctor['fonction']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Expérience</h5>
+                <p><?php echo $doctor['experience']?></p>
+            </div>
+            <div class="form-group">
+                <h5>Inscription</h5>
+                <p><?php echo $doctor['created']?></p>
+            </div>
+        </div>
+    </div>
+    <a class='btn btn-primary' href='./doctors.php'>Retour</a>
+<?php }?>
--- admin/includes/view_user.php
+++ admin/includes/view_user.php
@@ -1,10 +1,75 @@
 
 <?php 
-$user = getUserbyPseudo($_GET["p_id"]); 
+$user = getUserbyId($_GET["p_id"]); 
 
 if(!$user){
-    echo "<h4 class='alert alert-warning'>L'utilisateur n'est pas reconnu</h4>";
-    echo "<p><a class='btn btn-secondary' href='./users.php'>Retour</a></p>";
-}
-
-?>
+    echo "<h4 class='error-box'>L'utilisateur n'est pas reconnu</h4>";
+    echo "<p><a class='btn btn-primary' href='./users.php'>Retour</a></p>";
+}else{
+    ?>
+    <style>
+        .form-group{
+            font-size:1.2em;
+        }
+        h5{
+            font-size:1.5em;
+            font-weight:bold;
+        }
+    </style>
+    <div class="row text-center" style="background-color:white;padding:2em;">
+            <div class="col-md-12 col-lg-2">
+                <img src="./../profiles/<?php echo $user['picture'] ?>" style="width: 120px;height: 120px;object-fit: cover;">
+                <hr>
+            </div>
+            <div class="col-md-12 col-lg-5">
+                <div class="form-group">
+                    <h5>Nom</h5>
+                    <p><?php echo $user['first_name']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Prénom</h5>
+                    <p><?php echo $user['last_name']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Sexe</h5>
+                    <p><?php echo $user['sexe']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Email</h5>
+                    <p><?php echo $user['email']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Pseudo</h5>
+                    <p><?php echo $user['pseudo']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Contact</h5>
+                    <p><?php echo $user['contact']?></p>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-5">
+            
+            <div class="form-group">
+                    <h5>Profession</h5>
+                    <p><?php echo $user['profession']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Groupe Sanguin</h5>
+                    <p><?php echo $user['blood']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Taille</h5>
+                    <p><?php echo $user['height']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Poids</h5>
+                    <p><?php echo $user['weight']?></p>
+                </div>
+                <div class="form-group">
+                    <h5>Inscription</h5>
+                    <p><?php echo $user['created']?></p>
+                </div>
+            </div>
+        </div>
+        <a class='btn btn-primary' href='./users.php'>Retour</a>
+    <?php }?>

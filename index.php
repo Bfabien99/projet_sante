5c01471f0876a91ae13ee589a5b846ea60c3ccb6
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-08-26 14:38:59 +0000

    heroku test

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
 // Redirection en fonction de la route
-if(isset($_GET['route']) && !empty($_GET['route'])){
-    switch($_GET['route']){
-        case 'login':
-            include 'includes/login_redirect.php';
-            break;
-        case 'register':
-            include 'includes/register.php';
-            break;
-        default:
-            include 'includes/home.php';
-    }
-}
-else{
-    include 'includes/home.php';
-}
+echo "a";
 ?>

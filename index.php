3a16f11399137ea6f3ecdec844d81a3ffb8d6d91
Bfabien99 <fabienbrou99@gmail.com>
2022-08-27 12:02:47 +0000

    ...

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 // Redirection en fonction de la route
-
-    var_dump($_SERVER);
-
+if(isset($_GET['route']) && !empty($_GET['route'])){
+    switch($_GET['route']){
+        case 'login':
+            include './includes/login_redirect.php';
+            break;
+        case 'register':
+            include './includes/register.php';
+            break;
+        default:
+            include './includes/home.php';
+    }
+}
+else{
+    include './includes/home.php';
+}
 ?>
